a3c48de6fc3c41e7b16510c9424b5bfbef5f99e2
MoloZzz <mega_oleksii@ukr.net>
2025-02-13 16:40:21 +0200

    style: format

--- story-love/src/app.module.ts
+++ story-love/src/app.module.ts
@@ -3,14 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: [`src/.env`],
-    validationSchema: Joi.object({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`src/.env`],
+      validationSchema: Joi.object({
         API_DOCS_ENABLED: Joi.string().optional(),
         PORT: Joi.number().required(),
+      }),
     }),
-}),],
+  ],
   controllers: [],
   providers: [],
 })
