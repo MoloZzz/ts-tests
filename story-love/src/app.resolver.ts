36fb8e9c5a4cd57fd643f3567888aabfde466343
MoloZzz <mega_oleksii@ukr.net>
2025-02-14 17:39:18 +0200

    style: non-tech materials

--- story-love/src/app.resolver.ts
+++ story-love/src/app.resolver.ts
@@ -3,7 +3,7 @@ import { Query, Resolver } from '@nestjs/graphql';
 @Resolver()
 export class AppResolver {
   @Query(() => String)
-  ping(): string {
+  async ping(): Promise<string> {
     return 'pong';
   }
 }
